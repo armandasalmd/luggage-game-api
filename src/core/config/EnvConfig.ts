bcc26abdff35c2381dfa5d62a3bf74af769c0fcc
Armandas Barkauskas (barkausa) <barkausa@coventry.ac.uk>
2022-04-19 17:16:16 +0100

    Cors origin

--- src/core/config/EnvConfig.ts
+++ src/core/config/EnvConfig.ts
@@ -7,7 +7,7 @@ export class EnvConfig extends BaseConfig {
   public static allowedOrigins = [
     EnvConfig.productionDomain,
     EnvConfig.developmentDomain,
-    "https://luggage-game-1uoitvxv7-armandasbark.vercel.app",
+    "https://luggage-game-qyotz98cf-armandasbark.vercel.app",
   ];
 
   public get clientHostName(): string {
